a9f81fe9bb3778558cfa2a263a564f4a8a223689
hk6876c <hk6876c@gre.ac.uk>
2024-02-09 03:11:43 +0000

    Works well for a single client now. Need to implement multi-threading to allow multiple user

--- Assesment/src/codex/Client.java
+++ Assesment/src/codex/Client.java
@@ -55,7 +55,7 @@ public class Client {
                 if (line.startsWith("SUBMITNAME")) {
                     out.println(id);
                 } else if (line.startsWith("NAMEACCEPTED")) {
-                    this.frame.setTitle("Chatter - " + id);
+                    this.frame.setTitle("Chatter - " + id );
                     textField.setEditable(true);
                 } else if (line.startsWith("MESSAGE")) {
                     messageArea.append(line.substring(8) + "\n");
