c8bddfdb45d077ed2ce895bdab270450419c7b5e
hk6876c <hk6876c@gre.ac.uk>
2024-02-09 01:25:29 +0000

    Created a basic date server

---
+++ Assesment/src/codex/Server.java
@@ -0,0 +1,22 @@
+package codex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Server {
+	public static void main(String[] args) throws IOException { 
+		try (ServerSocket listener = new ServerSocket(7000)) {
+			System.out.println("The date server is running..."); 
+			while (true) {
+				try (Socket socket = listener.accept()) { 
+					PrintWriter out = 
+							new PrintWriter(socket.getOutputStream(), true);
+					out.println(new Date().toString());
+					}
+				}
+		}
+	}
+}
